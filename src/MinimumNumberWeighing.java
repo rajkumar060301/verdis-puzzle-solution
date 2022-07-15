b29d629e57c469be5850810885888aae9696b247
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2022-07-15 22:08:52 +0530

    Minimum number weighing

--- src/MinimumNumberWeighing.java
+++ src/MinimumNumberWeighing.java
@@ -1,2 +1,40 @@
-public class MinimumNumberCoinRequred {
+/*
+ * Author Name: Raj Kumar
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * JDK: 18 version
+ * Date: 15-Jul-22
+ */
+
+/*Problem-B
+        The input is the number of coins and output should be the minimum number weighing required.
+*/
+public class MinimumNumberWeighing {
+    // m is size of coins array (number of different coins)
+    static int minCoins(int coins[], int m, int V) {
+        // base case
+        if (V == 0) return 0;
+
+        // Initialize result
+        int res = Integer.MAX_VALUE;
+
+        // Try every coin that has smaller value than V
+        for (int i = 0; i < m; i++) {
+            if (coins[i] <= V) {
+                int sub_res = minCoins(coins, m, V - coins[i]);
+
+                // Check for INT_MAX to avoid overflow and see if
+                // result can minimized
+                if (sub_res != Integer.MAX_VALUE && sub_res + 1 < res)
+                    res = sub_res + 1;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String args[]) {
+        int coins[] = {9, 6, 5, 1};
+        int m = coins.length;
+        int V = 11;
+        System.out.println("Minimum coins required is " + minCoins(coins, m, V));
+    }
 }
